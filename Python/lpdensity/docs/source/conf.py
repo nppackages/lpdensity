0be32c5059f07a46306990b4051358e85ee6e54b
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2022-05-26 11:56:05 -0400

    updated documentation

--- Python/lpdensity/docs/source/conf.py
+++ Python/lpdensity/docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/lpdensity'))
+sys.path.insert(0, os.path.abspath('../../src/'))
+autodoc_mock_imports = ["numpy", "scipy", "pandas", "sympy", "statistics", "plotnine"]
 
 # -- Project information -----------------------------------------------------
 
@@ -45,7 +46,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
