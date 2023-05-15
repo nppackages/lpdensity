221ddceed950bb00a17125ccb8f08194bcc555a2
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2023-05-15 14:00:03 -0400

    fixed summary table output

--- Python/lpdensity/src/lpdensity/lpdensity.py
+++ Python/lpdensity/src/lpdensity/lpdensity.py
@@ -20,7 +20,7 @@ def lpdensity(data, grid=None, bw=None, p=None, q=None, v=None,
 	massPoints=True, bwselect='mse-dpi',
 	stdVar=True, regularize=True,nLocalMin=None, nUniqueMin=None,
 	Cweights=None, Pweights=None):
-	
+
 	"""
 	Parameters
 	----------
@@ -56,7 +56,7 @@ def lpdensity(data, grid=None, bw=None, p=None, q=None, v=None,
 		Numeric, specifies the weights used for counterfactual distribution construction. Should have the same length as the data.
 	Pweights : numeric vector
 		Numeric, specifies the weights used in sampling. Should have the same length as the data.
-	
+
 	Returns
 	-------
 	Estimate:
@@ -66,22 +66,22 @@ def lpdensity(data, grid=None, bw=None, p=None, q=None, v=None,
 	CovMat_q :
 		The variance-covariance matrix corresponding to *f_q*.
 	"""
-	
+
 	##############################
 	##   INPUT ERROR HANDLING   ##
 	##############################
-	
+
 	#data
 	data = pd.DataFrame(data)
 	if data.isnull().values.any():
 		raise Exception(data.isnull().sum() + 'missing observation(s) are ignored.\n')
 		data = data.dropna(axis=0)
-	
+
 	n = len(data)
 	data = np.array(data).reshape(n,)
 	if n==0 or not np.isscalar(n):
 		raise Exception("Data should be numeric, and cannot be empty.")
-	
+
 	#grid
 	if grid is None:
 		grid = np.quantile(data, np.arange(0.05, 0.95, 0.05))
@@ -128,8 +128,8 @@ def lpdensity(data, grid=None, bw=None, p=None, q=None, v=None,
 			raise Exception("Bandwidth has to be the same length as grid.")
 		else:
 			bwselect = 'user provided'
-			
-	
+
+
 	#kernel
 	if kernel is None:
 		flag_no_kernel = True
@@ -146,10 +146,10 @@ def lpdensity(data, grid=None, bw=None, p=None, q=None, v=None,
 		raise Exception("Counterfactual weights incorrectly specified.")
 	elif len(Cweights) !=n:
 		raise Exception("Counterfactual weights should have the same length as sample.")
-	
+
 	if isinstance(Cweights, pd.Series):
 		Cweights = Cweights.to_numpy()
-	
+
 	# Pweights
 	if Pweights is None:
 		Pweights = np.repeat(1,n)
@@ -159,10 +159,10 @@ def lpdensity(data, grid=None, bw=None, p=None, q=None, v=None,
 		raise Exception("Probability weights should have the same length as sample.")
 	elif any(x<0 for x in Pweights)==True:
 		raise Exception("Probability weights should be nonnegative.")
-	
+
 	if isinstance(Pweights, pd.Series):
 		Pweights = Pweights.to_numpy()
-	
+
 	# scale
 	if scale is None:
 		scale = 1
@@ -214,7 +214,7 @@ def lpdensity(data, grid=None, bw=None, p=None, q=None, v=None,
 	#########################
 	##   Sample Trimming   ##
 	#########################
-	
+
 	#	trim_index = (Pweights == 0)
 #	if np.all(trim_index==True):
 #		raise Exception("All weights are zero.")
@@ -239,24 +239,24 @@ def lpdensity(data, grid=None, bw=None, p=None, q=None, v=None,
 	else:
 		bw = lpbwdensity_fn.__bw_IROT(data=data, grid=grid, p=p, v=v, kernel=kernel, Cweights=Cweights, Pweights=Pweights, massPoints=massPoints, stdVar=stdVar, regularize=regularize, nLocalMin=nLocalMin, nUniqueMin=nUniqueMin)
 		bw = np.repeat(bw, len(grid))
-		
+
 	##########################
 	##   Point Estimation   ##
 	##########################
 	Temp_Result = lpdensity_fn.__lpdensity_fn(data=data, grid=grid, bw=bw, p=p, q=q, v=v, kernel=kernel,Cweights=Cweights, Pweights=Pweights, massPoints=massPoints, showSE=True)
-	
+
 	#scaling output
 	Temp_Result['Estimate'][['f_p', 'f_q', 'se_p', 'se_q']] = Temp_Result['Estimate'][['f_p', 'f_q', 'se_p', 'se_q']] * scale
 	Temp_Result['CovMat_p'] = Temp_Result['CovMat_p']* np.power(scale, 2)
 	Temp_Result['CovMat_q'] = Temp_Result['CovMat_q']* np.power(scale, 2)
 
-	
+
 	#################################
 	##   Return lpdensity Object   ##
 	#################################
-	
+
 	lpoutput = lpdensity_output(Temp_Result['Estimate'], Temp_Result['CovMat_p'], Temp_Result['CovMat_q'], p, q, v, kernel, scale, massPoints, n, ng, bwselect, stdVar, regularize, nLocalMin, nUniqueMin, data_min = min(data), data_max = max(data), grid_min = min(grid), grid_max = max(grid))
-	
+
 	return(lpoutput)
 
 
@@ -265,7 +265,7 @@ def lpdensity(data, grid=None, bw=None, p=None, q=None, v=None,
 class lpdensity_output:
 	"""
 	Class of lpdensity function outputs.
-	
+
 	Object type returned by :py:meth:`~lpdensity`.
 	"""
 	def __init__(self, Estimate, CovMat_p, CovMat_q, p, q, v, kernel, scale, massPoints, n, ng, bwselect, stdVar, regularize, nLocalMin, nUniqueMin, data_min, data_max, grid_min, grid_max):
@@ -289,7 +289,7 @@ class lpdensity_output:
 		self.data_max = data_max
 		self.grid_min = grid_min
 		self.grid_max = grid_max
-	
+
 	#summary output
 	def __repr__(self):
 		print('Call: lpdensity')
@@ -304,7 +304,7 @@ class lpdensity_output:
 		print('Scaling factor:'.ljust(fw), str(self.scale).rjust(25))
 		print('Bandwidth Method:'.ljust(fw), str(self.bwselect).rjust(25))
 		print('')
-		
+
 		CIuniform=False
 		alpha=0.05
 		#critical value
@@ -325,17 +325,22 @@ class lpdensity_output:
 					normalSimul = np.random.multivariate_normal(mean = np.repeat(0, len(self.CovMat_q)), cov = corrMat, size=2000)
 					#comput estimated quantile
 					z_val = np.quantile(list(map(lambda x: max(abs(x)), normalSimul)), 1-alpha)
-					
+
 				else:
 					#exception for non-PSD covariance matrix
 					raise warnings.warn('Variance-Covariance is not positive semidefinite. Will only plot pointwise confidence intervals.')
 					z_val = spstat.norm.ppf(1-alpha/2)
 		else:
 			z_val = spstat.norm.ppf(1-alpha/2)
-		
-		ci_df['CI_l_q'] = self.Estimate['f_q'] - z_val * self.Estimate['se_q']
-		ci_df['CI_r_q'] = self.Estimate['f_q'] + z_val * self.Estimate['se_q']
-		
+
+		if self.p == self.q:
+			ci_df['CI_l_q'] = self.Estimate['f_p'] - z_val * self.Estimate['se_p']
+			ci_df['CI_r_q'] = self.Estimate['f_p'] + z_val * self.Estimate['se_p']
+		else:
+			ci_df['CI_l_q'] = self.Estimate['f_q'] - z_val * self.Estimate['se_q']
+			ci_df['CI_r_q'] = self.Estimate['f_q'] + z_val * self.Estimate['se_q']
+
+
 		#Table
 		fw_l = 15
 		fw_c = 8
@@ -350,7 +355,7 @@ class lpdensity_output:
 			str(round(self.Estimate['bw'][j], n_dec)).ljust(fw_c), str(int(self.Estimate['nh'][j])).ljust(fw_c), str(int(self.Estimate['nhu'][j])).ljust(fw_c), str(round(self.Estimate['f_p'][j], n_dec)).rjust(fw_c), str(round(self.Estimate['se_p'][j], n_dec)).rjust(fw_c), str('['+str(round(ci_df['CI_l_q'][j], n_dec)) + ',' + str(round(ci_df['CI_r_q'][j], n_dec))+']').rjust(fw_l))
 		print('='*81)
 		return('')
-		
+
 	#print call
 	def __str__(self):
 		print('Call: lpdensity')
@@ -367,7 +372,7 @@ class lpdensity_output:
 		print('')
 		print('Use .repr() to show estimates.')
 		return ('')
-	
+
 	#coefficients of estimation
 	def coef(self):
 		"""
@@ -375,7 +380,7 @@ class lpdensity_output:
 		"""
 		coef_data = self.Estimate[['grid', 'f_p', 'f_q']]
 		return(coef_data)
-	
+
 	#variance-covariance
 	def vcov(self):
 		"""
@@ -383,12 +388,12 @@ class lpdensity_output:
 		"""
 		vcov_data = self.Estimate[['grid', 'se_p', 'se_q']]
 		return({'stdErr': vcov_data, 'CovMat_p': self.CovMat_p, 'CovMat_q': self.CovMat_q})
-	
+
 	#confidence interval function
 	def confint(self, alpha=0.05, CIuniform=False, CIsimul=2000):
 		"""
 		Returns confindence intervals/bands for prespecified confidence level.
-		
+
 		alpha : number
 			Confindence level, must be between 0 and 1.
 		CIuniform : boolean
@@ -398,14 +403,14 @@ class lpdensity_output:
 		"""
 		if alpha<0 or alpha>1:
 			raise Exception("Significance level incorrectly specified.")
-		
+
 		#initialized output DF
 		estimate_df = pd.DataFrame(columns=['grid', 'f_p', 'CI_l_p', 'CI_r_p', 'f_q', 'CI_l_q', 'CI_r_q'])
-		
+
 		estimate_df['grid'] = self.Estimate['grid']
 		estimate_df['f_p'] = self.Estimate['f_p']
 		estimate_df['f_q'] = self.Estimate['f_q']
-		
+
 		#critical value
 		if CIuniform==True:
 			if np.isnan(CIsimul):
@@ -423,29 +428,29 @@ class lpdensity_output:
 					normalSimul = np.random.multivariate_normal(mean = np.repeat(0, len(self.CovMat_q)), cov = corrMat, size=CIsimul)
 					#comput estimated quantile
 					z_val = np.quantile(list(map(lambda x: max(abs(x)), normalSimul)), 1-alpha)
-					
+
 				else:
 					#exception for non-PSD covariance matrix
 					raise warnings.warn('Variance-Covariance is not positive semidefinite. Will only plot pointwise confidence intervals.')
 					z_val = spstat.norm.ppf(1-alpha/2)
 		else:
 			z_val = spstat.norm.ppf(1-alpha/2)
-		
+
 		estimate_df['CI_l_p'] = self.Estimate['f_p'] - z_val * self.Estimate['se_p']
 		estimate_df['CI_r_p'] = self.Estimate['f_p'] + z_val * self.Estimate['se_p']
-		
+
 		estimate_df['CI_l_q'] = self.Estimate['f_q'] - z_val * self.Estimate['se_q']
 		estimate_df['CI_r_q'] = self.Estimate['f_q'] + z_val * self.Estimate['se_q']
-		
+
 		return(estimate_df)
-	
+
 	#plotting function
 	def plot(self, alpha=0.05, type='line', CItype='region', CIuniform=False, CIsimul=2000, hist=False, histData=None, histBins=None, histFillCol=3, histFillShade=0.2, histLineCol="white", title=None, xlabel=None, ylabel=None, CIshade=0.2):
-	
+
 		"""
 		Method to plot estimate and confidence bands.
 		Requires ggplot.
-		
+
 		alpha : number
 			Confindence level, must be between 0 and 1.
 		CIuniform : boolean
@@ -459,7 +464,7 @@ class lpdensity_output:
 		"""
 		if self.ng <2:
 			raise Exception("At least two grid points are needed to plot input.")
-			
+
 		if alpha<0 or alpha>1:
 			raise Exception("Significance level incorrectly specified.")
 		#TODO: error handling
@@ -470,10 +475,10 @@ class lpdensity_output:
 				temp_plot = (pn.ggplot(histData, pn.aes(x='d1')) + pn.geom_histogram(bins=histBins) + pn.theme_bw())
 		else:
 			temp_plot = pn.ggplot() + pn.theme_bw()
-		
-		
+
+
 		data_x = self.Estimate[['grid', 'f_p', 'f_q', 'se_p', 'se_q']]
-		
+
 		#critical value
 		if CIuniform==True:
 			if np.isnan(CIsimul):
@@ -491,41 +496,41 @@ class lpdensity_output:
 					normalSimul = np.random.multivariate_normal(mean = np.repeat(0, len(self.CovMat_q)), cov = corrMat, size=CIsimul)
 					#comput estimated quantile
 					z_val = np.quantile(list(map(lambda x: max(abs(x)), normalSimul)), 1-alpha)
-					
+
 				else:
 					#exception for non-PSD covariance matrix
 					raise warnings.warn('Variance-Covariance is not positive semidefinite. Will only plot pointwise confidence intervals.')
 					z_val = spstat.norm.ppf(1-alpha/2)
 		else:
 			z_val = spstat.norm.ppf(1-alpha/2)
-				
+
 		data_x['CI_l'] = data_x['f_q'] - z_val * data_x['se_q']
 		data_x['CI_r'] = data_x['f_q'] + z_val * data_x['se_q']
-		
+
 		if CItype in ['region', 'all']:
 			temp_plot = temp_plot + pn.geom_ribbon(data=data_x, mapping=pn.aes(x='grid', ymin='CI_l', ymax='CI_r'), alpha=CIshade)
-		
+
 		if CItype in ['lines', 'all']:
 			temp_plot = temp_plot + pn.geom_line(data=data_x, mapping=pn.aes(x='grid', y='CI_l')) + pn.geom_line(data=data_x, mapping=pn.aes(x='grid', y='CI_r'))
-			
+
 		if CItype in ['ebar', 'all']:
 			temp_plot = temp_plot + pn.geom_errorbar(data=data_x, mapping=pn.aes(x='grid', ymin='CI_l', ymax='CI_r'))
-		
+
 		if type in ['line', 'both']:
 			temp_plot = temp_plot + pn.geom_line(data=data_x, mapping=pn.aes(x='grid', y='f_p'))
-		
+
 		if type in ['points', 'both']:
 			temp_plot = temp_plot + pn.geom_point(data=data_x, mapping=pn.aes(x='grid', y='f_p'))
-		
+
 		if xlabel is None:
 			xlabel = ''
-		
+
 		if ylabel is None:
 			ylabel = ''
-		
+
 		if title is None:
 			title = ''
-			
+
 		temp_plot = temp_plot + pn.xlab(xlabel) + pn.ylab(ylabel)
-		
+
 		return(temp_plot)
