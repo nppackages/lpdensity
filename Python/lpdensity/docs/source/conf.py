8cf50e193e495896b09b7e81e46c9f16f734acd9
Rajita Chandak <rajitachandak@dynamic-oit-vapornet100-10-9-149-160.princeton.edu>
2024-09-26 15:36:48 +0200

    updated for compatibility with numpy2.0

--- Python/lpdensity/docs/source/conf.py
+++ Python/lpdensity/docs/source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/'))
+
+sys.path.insert(0, os.path.abspath("../../src/"))
 autodoc_mock_imports = ["numpy", "scipy", "pandas", "sympy", "statistics", "plotnine"]
 
 # -- Project information -----------------------------------------------------
 
-project = 'lpdensity'
-copyright = '2021, Rajita Chandak'
-author = 'Rajita Chandak'
+project = "lpdensity"
+copyright = "2021, Rajita Chandak"
+author = "Rajita Chandak"
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.3'
+release = "1.0.3"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,10 +31,10 @@ release = '1.0.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -46,9 +47,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
--- Python/lpdensity/tests/example.py
+++ Python/lpdensity/tests/example.py
@@ -7,39 +7,64 @@ import statistics as stat
 import plotnine as pn
 
 jtpa = pd.read_csv("jtpa.csv")
-X = jtpa[['income', 'hsorged', 'male', 'nonwhite', 'married', 'wkless13', 'afdc', 'age2225', 'age2629', 'age3035', 'age3644', 'age4554']]
+X = jtpa[
+    [
+        "income",
+        "hsorged",
+        "male",
+        "nonwhite",
+        "married",
+        "wkless13",
+        "afdc",
+        "age2225",
+        "age2629",
+        "age3035",
+        "age3644",
+        "age4554",
+    ]
+]
 
-n = len(jtpa['income'])
-ones_df = pd.DataFrame(np.repeat(1,n))
+n = len(jtpa["income"])
+ones_df = pd.DataFrame(np.repeat(1, n))
 X = pd.concat([X, ones_df], axis=1)
 
-jtpa_notreat = jtpa.loc[jtpa['treatment'] == 0]
+jtpa_notreat = jtpa.loc[jtpa["treatment"] == 0]
 grid = np.linspace(2, 5, 10)
 
-#uniquedata = lpdensityUnique(jtpa_notreat['logincome'])
-#print(len(uniquedata['unique']))
-#print(len(uniquedata['index']))
-#print(len(uniquedata['freq']))
+# uniquedata = lpdensityUnique(jtpa_notreat['logincome'])
+# print(len(uniquedata['unique']))
+# print(len(uniquedata['index']))
+# print(len(uniquedata['freq']))
 
-#n = len(data)
-#grid = np.linspace(min(data), max(data), 19)
-p=2
-q=p+1
-v=1
-kernel= "uniform"
-Cweights=np.repeat(1, n)
-Pweights=np.repeat(1, n)
-massPoints=True
-stdVar=True
-regularize=True
+# n = len(data)
+# grid = np.linspace(min(data), max(data), 19)
+p = 2
+q = p + 1
+v = 1
+kernel = "uniform"
+Cweights = np.repeat(1, n)
+Pweights = np.repeat(1, n)
+massPoints = True
+stdVar = True
+regularize = True
 nLocalMin = 20 + p + 1
 nUniqueMin = 20 + p + 1
 alpha = 0.05
 
-est = lpdensity(data=jtpa_notreat['logincome'], bwselect='imse-dpi', grid=grid, kernel="triangular")
-est2 = lpdensity(data=jtpa_notreat['logincome'], bwselect="imse-dpi", grid=grid, Cweights=jtpa_notreat['hsorged'])
-est3 = lpdensity(data=jtpa_notreat['logincome'], bwselect="imse-dpi", grid=grid, Cweights=jtpa_notreat['hsorged'])
+est = lpdensity(
+    data=jtpa_notreat["logincome"], bwselect="imse-dpi", grid=grid, kernel="triangular"
+)
+est2 = lpdensity(
+    data=jtpa_notreat["logincome"],
+    bwselect="imse-dpi",
+    grid=grid,
+    Cweights=jtpa_notreat["hsorged"],
+)
+est3 = lpdensity(
+    data=jtpa_notreat["logincome"],
+    bwselect="imse-dpi",
+    grid=grid,
+    Cweights=jtpa_notreat["hsorged"],
+)
 print(type(est3))
 print(est3.plot(CIuniform=True))
-
-
